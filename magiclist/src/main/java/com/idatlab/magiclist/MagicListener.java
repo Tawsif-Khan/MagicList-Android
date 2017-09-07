8cae25ff83267e10afb6c23129c53e7fde2b13dc
Tawsif-Khan <tawsif.online@gmail.com>
2017-09-07 17:35:48 +0800

    init

---
+++ magiclist/src/main/java/com/idatlab/magiclist/MagicListener.java
@@ -0,0 +1,12 @@
+package com.idatlab.magiclist;
+
+import android.view.View;
+
+/**
+ * Created by user2 on 8/28/17.
+ */
+
+public interface MagicListener {
+    void onMagicClick(View view, int position);
+
+}
